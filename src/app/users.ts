e30aaacceac9232c0f7aee30ecf3ebccccbd0c6b
monicaoyugi <monicaoyugi@gmail.com>
2020-01-20 08:15:34 +0300

    exported users classes credentials

--- src/app/users.ts
+++ src/app/users.ts
@@ -1,2 +1,12 @@
 export class Users {
+  avatar_url: any;
+  login: any;
+  bio: any;
+  name: any;
+  company: any;
+  location: any;
+  followers: any;
+  following: any;
+  created_at: Date;
+  public_repos: any;
 }
