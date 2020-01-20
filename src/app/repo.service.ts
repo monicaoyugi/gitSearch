5f2349ce288d410d3944b86a2f55527dfa6249b7
monicaoyugi <monicaoyugi@gmail.com>
2020-01-20 09:21:13 +0300

    generated repo-services

---
+++ src/app/repo.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable, from} from 'rxjs';
+import {environment} from '../environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RepoService {
+
+  constructor() { }
+}
